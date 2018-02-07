f216306aeb4256b734f098dc97076ee0cf69cdbd
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-02-07 11:36:18 +0800

    First Commit: API call not working

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,14 @@
-const API_ID = process.env.REACT_APP_API_ID
-const APP_KEY = process.env.REACT_APP_APP_KEY
+import { REACT_APP_API_ID, REACT_APP_API_ID } from './keys';
 
-export function fetchRecipes (food = '') {
-  food = food.trim()
+const API_ID = REACT_APP_API_ID;
+const APP_KEY = REACT_APP_APP_KEY;
 
-  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
-    .then((res) => res.json())
-    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+export function fetchRecipes(food = '') {
+  food = food.trim();
+  console.log(API_ID);
+  return fetch(
+    `https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`
+  )
+    .then(res => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe));
 }
---
+++ src/utils/keys.js
@@ -0,0 +1,2 @@
+export const REACT_APP_API_ID = '37d6dc37';
+export const REACT_APP_APP_KEY = 'f5e200ecb2b5c5a7ef4447ad50599638';
