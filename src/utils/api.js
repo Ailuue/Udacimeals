11d2cd79adeca4ba8bbf1c568cc998f1ee443f99
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-02-08 09:55:32 +0800

    feat: Apply middleware.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,10 @@
-import { REACT_APP_API_ID, REACT_APP_API_ID } from './keys';
+const API_ID = process.env.REACT_APP_API_ID
+const APP_KEY = process.env.REACT_APP_APP_KEY
 
-const API_ID = REACT_APP_API_ID;
-const APP_KEY = REACT_APP_APP_KEY;
+export function fetchRecipes (food = '') {
+  food = food.trim()
 
-export function fetchRecipes(food = '') {
-  food = food.trim();
-  console.log(API_ID);
-  return fetch(
-    `https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`
-  )
-    .then(res => res.json())
-    .then(({ hits }) => hits.map(({ recipe }) => recipe));
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+    .then((res) => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe))
 }
